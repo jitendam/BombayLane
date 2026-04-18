d318626c400f43e05af0d9607ef30522c12944c2
jitendam <69076855+jitendam@users.noreply.github.com>
2026-04-18 10:38:51 +0530

    Create MenuItem model file

---
+++ models/MenuItem.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const menuItemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    available: {
+        type: Boolean,
+        default: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema);
+
+module.exports = MenuItem;
